691a5da8b11f9e0dffa06c26cca6c5f449356fb1
Marik28 <markel20002000@gmail.com>
2021-11-02 15:37:41 +0600

    добавил тудушку

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 // TODO добавить сохранение уже имеющихся расписания и заданий, например, в
 session storage,
+// TODO сделать навигаю с vue-router
 <template>
   <link
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
