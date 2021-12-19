04326b9e5009d9979afe9faab9879b23200d4b18
Marik28 <markel20002000@gmail.com>
2021-12-19 17:30:47 +0600

    add SidebarFilter

---
+++ src/components/SidebarFilter.vue
@@ -0,0 +1,25 @@
+<template>
+  <select v-model="selectedGroup" class="form-select">
+    <option v-for="group of groups" :key="group.id">
+      {{ group.name }}
+    </option>
+  </select>
+</template>
+<script>
+import "../models/schemas.js";
+export default {
+  data() {
+    return {
+      /**
+       * @type {Array<Group>}
+       */
+      groups: [
+        { id: 1, name: "kek" },
+        { id: 2, name: "kekw" },
+      ],
+      selectedGroup: null,
+    };
+  },
+  created() {},
+};
+</script>
